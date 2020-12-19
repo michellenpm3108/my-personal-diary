0fce16322760c0fb3313b33a2c37202addb8509c
Michelle <michelle.npm3108@gmail.com>
2020-12-20 06:35:06 +0800

    add entry button

--- index.js
+++ index.js
@@ -2,24 +2,19 @@ const entryForm = document.getElementById('entry-form')
 const entryTextbox = document.getElementById('entry-textbox')
 const submitBtn = document.querySelector('btn')
 const entrySection = document.getElementById('entry-section')
+const entryBtnSection = document.getElementById('entryBtnSection')
 
-
-// function addEntryToDom(event){
-//     event.preventDefault()
-//     let newEntry = document.createElement('div')
-//     newEntry.className = 'single-entry'
-//     newEntry.textContent= entryTextbox.value
-//     entrySection.append(newEntry)
-// }
-// entryForm.addEventListener('submit', addEntryToDom)
 
 function addEntryToDom(event) {
     event.preventDefault()
     let newEntryDiv = document.createElement('div')
     newEntryDiv.textContent = entryTextbox.value
     newEntryDiv.className = 'new-entry'
-    // newEntryDiv.style.display = 'none'
+    newEntryDiv.style.display = 'none'
     entrySection.append(newEntryDiv)
     entryTextbox.value = ''
+    let entryBtn = document.createElement('button')
+    entryBtn.className = 'entryBtn'
+    entryBtnSection.append(entryBtn)
 }
 entryForm.addEventListener('submit', addEntryToDom)
