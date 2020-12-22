e736b13445c4a27f7051c0c103eabf5a4a2f7ae4
Michelle <michelle.npm3108@gmail.com>
2020-12-23 07:23:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ const entryForm = document.getElementById('entry-form')
 const entryTextBox = document.querySelector('.entry-textbox')
 const entrySection = document.querySelector('.entry-section')
 const entryBtnSection = document.querySelector('.entry-btn-section')
+
 let count = 1
+
 entryForm.addEventListener('submit', function(event){
     event.preventDefault(event)
+
     let entryDiv = document.createElement('div')
     entryDiv.textContent = entryTextBox.value
+    entryDiv.className='single-entry'
     entryDiv.style.display='none'
     entrySection.append(entryDiv)
+
+    entryTextBox.value=''
+
     let entryBtn = document.createElement('button')
+    entryBtn.className='entry-btn'
     entryBtn.textContent = 'day' + count
     count++
     entryBtnSection.append(entryBtn)
+
     entryBtn.addEventListener('click', function(){
+        const allEntries = document.querySelectorAll('.single-entry')
+        for (let i=0 ; i<allEntries.length ; i++) {
+            allEntries[i].style.display='none'
+        }
         entryDiv.style.display='block'
     })
 })
